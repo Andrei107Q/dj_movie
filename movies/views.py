a8092644e817a2d90b52dc99ea0f0c5a2d90025e
Andrei <denisenko.andrei.da@gmail.com>
2020-04-25 21:49:17 +0300

    part 9

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import *
+from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -31,4 +32,16 @@ class MovieDetailView(DetailView):
 #     def get(self, request, slug):
 #         movie = Movie.objects.get(url=slug)
 #         return render(request, 'movies/moviesingle.html', {'movie': movie})
-    
+
+
+class AddReview(View):
+    # Отзывы
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)    # узнаем к какому фильму привузять отзыв
+        if form.is_valid():     # проверяем валидность формы полученной с сайта
+            form =form.save(commit=False)   # приостановили сохранение формы
+            form.movie = movie     # передает id филтма
+            form.save()
+        return redirect(movie.get_absolute_url())   # формируем адрес страницы на которой оставили отзыв
+
