e2b8e7d75d8ae9b61606f3675db84228497ab3ee
Andrei <denisenko.andrei.da@gmail.com>
2020-05-02 23:34:32 +0300

    part 15

--- movies/views.py
+++ movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 
@@ -11,6 +11,11 @@ class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = 'movies/movies.html'
+
+    # def get_context_date(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context["categories"] = Category.objects.all()
+    #     return context
 
 
 # class MoviesView(View):
