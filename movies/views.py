fa3f22db5ed5be3a6261f1de836ac21fe8e2301a
Andrei <denisenko.andrei.da@gmail.com>
2020-05-03 18:53:34 +0300

    part 17

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,22 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+    # жинры и года выхода фильмов
+    def get_genre(self):
+        return Genre.objects.all()
+
+    def get_year(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     # Список фильмов
     model = Movie
     queryset = Movie.objects.filter(draft=False)
@@ -25,7 +35,7 @@ class MoviesView(ListView):
 #         return render(request, 'movies/movies.html', {'movie_list': movies})
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     # Описание фильма
     model = Movie
     slug_field = 'url'
@@ -53,11 +63,23 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())   # формируем адрес страницы на которой оставили отзыв
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     # выводи информацию об актере
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = 'name'     # поле по которому проходит поиск
+
+
+class FilterMoviesView(GenreYear, ListView):
+    # филтр фильмов
+    template_name = 'movies/movies.html'
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+        return queryset
 
 
 
