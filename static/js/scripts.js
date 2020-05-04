91b21ff72073fd671d872fd92772d0496bbfe55c
Andrei <denisenko.andrei.da@gmail.com>
2020-05-04 23:59:00 +0300

    part 17 test1

---
+++ static/js/scripts.js
@@ -0,0 +1,61 @@
+function ajaxSend(url, params) {
+    // отправляем запрос
+    fetch('${url}?{params}', {  // запрос на сервер с помощью 'fetch'
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+    })
+        .then(response => response.json()) // преобразуем промис в json
+        .then(json => render(json)) // json передаем в функцию render
+        .catch(error => console.error(error))   // вывод ошибки в консоль
+}
+
+const forms = document.querySelector('form[name=filter]'); // ищем форму по имени 'filter'
+
+forms.addEventListener('submit', function (e) { // когда вызываем метод 'submit' запускаем функцию
+    // получаем данные из формы
+    e.preventDefault(); // блокируем перезагрузку страницы
+    let url = this.action; // заносим информацию из формы в url
+    let params = new URLSearchParams(new FormDate(this)).toString();
+    ajaxSend(url, params);
+});
+
+function render(data) {
+    // Рендер шаблона
+    let template = Hogan.compile(html);
+    let output = template.render(data);
+
+    const div = document.querySelector('.left-ads-display>.row');
+    div.innerHTML = output;
+}
+
+let html = '\
+{{#movies}}\
+     <div class="col-md-4 product-men">\
+            <div class="product-shoe-info editContent text-center mt-lg-4">\
+                <div class="men-thumb-item">\
+                    <img src='media/{{ poster }}' class="img-fluid" alt="">\
+                </div>\
+                <div class="item-info-product">\
+                    <h4 class="">\
+                        <a href="/{{ url }}" class="editContent">\
+                            {{ title }}\
+                        </a>\
+                    </h4>\
+                    <div class="product_price">\
+                        <div class="grid-price">\
+                            <span class="money editContent">{{ tagline }}</span>\
+                        </div>\
+                    </div>\
+                    <ul class="stars">\
+                        <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>\
+                        <li><a href="#"><span class="fa fa-star" aria-hidden="true"></span></a></li>\
+                        <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>\
+                        <li><a href="#"><span class="fa fa-star-half-o" aria-hidden="true"></span></a></li>\
+                        <li><a href="#"><span class="fa fa-star-o" aria-hidden="true"></span></a></li>\
+                    </ul>\
+                </div>\
+            </div>\
+        </div>\
+{{/movies}}'
